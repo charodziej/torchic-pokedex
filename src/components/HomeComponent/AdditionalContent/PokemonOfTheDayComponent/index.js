53eb252d0e10279fc0cc05dd50f569a7fd7e75cb
Adenka <13adena@gmail.com>
2020-02-24 20:47:18 +0100

    Pokemon of The Day + Calendar

--- src/components/HomeComponent/AdditionalContent/PokemonOfTheDayComponent/index.js
+++ src/components/HomeComponent/AdditionalContent/PokemonOfTheDayComponent/index.js
@@ -5,26 +5,26 @@ import {withRouter} from 'react-router-dom';
 
 const classes = theme => ({
     card: {
-        width: 180,
-        margin: 10,
+        width: 250,
         boxShadow: 8,
-        borderRadius: 10
+        borderRadius: 5
     },
 
     pokeName: {
-        fontSize: 12,
+        fontSize: 24,
         textTransform: 'uppercase',
-        letterSpacing: '1.35mm',
-        textAlign: 'center',
-        alignItems: 'center',
+        letterSpacing: '2.7mm',
+        fontFamily: 'Indie Flower',
     },
 
-    properties: {
-        fontSize: 15,
-    }
+    subtitle: {
+        fontSize: 12,
+        textTransform: 'uppercase',
+        letterSpacing: '1mm',
+    },
 })
 
-class Pokemon extends React.PureComponent {
+class PokemonOfTheDay extends React.PureComponent {
     render() {
         const classes = this.props.classes
 
@@ -36,16 +36,18 @@ class Pokemon extends React.PureComponent {
                 onClick={() => this.props.handleOpen(this.props.pokemon.id-1)}
             >
                 <CardActionArea>
-                    <div style={{height: 170}}>
                     <CardMedia
                         component="img"
+                        style={{width: 150, marginLeft: 'auto', marginRight: 'auto', paddingTop: 10}}
                         alt={this.props.pokemon.name}
                         image={this.props.pokemon.img}  
                     />
-                    </div>
-                    <CardContent>
-                        <Typography className={classes.pokeName}>
+                    <CardContent style={{padding: 20}}>
+                        <Typography className = {classes.pokeName}>
                             {this.props.pokemon.name}
+                        </Typography>
+                        <Typography className = {classes.subtitle}>
+                            Pokemon Of The Day
                         </Typography>
                     </CardContent>
                 </CardActionArea>
@@ -54,4 +56,4 @@ class Pokemon extends React.PureComponent {
         )
     }
 }
-export default withRouter(withStyles(classes)(Pokemon))
+export default withRouter(withStyles(classes)(PokemonOfTheDay))
