4b25982ce50d1f742b6b767242dbf40999c8939e
Adenka <13adena@gmail.com>
2020-04-18 19:06:15 +0200

    Updates

--- precache-manifest.4836c6ce312cd001888834470a5052db.js
+++ precache-manifest.4836c6ce312cd001888834470a5052db.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "99c0a9aa3caa438f78130222ca09a0a7",
+    "revision": "ef6619c4888f279f66bf13fd9456ef01",
     "url": "/torchic-pokedex/index.html"
   },
   {
-    "revision": "349930493075a6ec301b",
+    "revision": "4c39fa4779930c7e132f",
     "url": "/torchic-pokedex/static/css/main.048da130.chunk.css"
   },
   {
-    "revision": "c7bf13102f2f4c757a09",
-    "url": "/torchic-pokedex/static/js/2.783d6f52.chunk.js"
+    "revision": "d78f29a506753aa1a76a",
+    "url": "/torchic-pokedex/static/js/2.0f7dc616.chunk.js"
   },
   {
-    "revision": "349930493075a6ec301b",
-    "url": "/torchic-pokedex/static/js/main.d400ba1b.chunk.js"
+    "revision": "4c39fa4779930c7e132f",
+    "url": "/torchic-pokedex/static/js/main.1bf94e01.chunk.js"
   },
   {
     "revision": "c48a89a7639ca8d33341",
